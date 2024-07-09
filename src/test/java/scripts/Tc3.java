cc183bf275f082314f35bcced97c0f232adfbadd
megha_100 <megha.sridhar1@gmail.com>
2024-07-09 14:54:48 +0530

    modified tc3

--- src/test/java/scripts/Tc3.java
+++ src/test/java/scripts/Tc3.java
@@ -1,11 +1,16 @@
 package scripts;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Tc3 {
 
+	
+@Test
 	public void m1()
 	
 	
 	{
-		
+		Reporter.log("hi",true);
 	}
 }
